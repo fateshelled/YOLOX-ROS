2184735bcfbb295d404f0b10a0757ecf4937e546
Ar-Ray <67567093+Ar-Ray-code@users.noreply.github.com>
2024-06-22 19:27:10 +0900

    remove --workspace option (for supporting latest trtexec)

--- weights/tensorrt/convert.bash
+++ weights/tensorrt/convert.bash
@@ -2,22 +2,15 @@
 
 # if $1 is empty
 if [ -z "$1" ]; then
-    echo "Usage: $0 <target-model> <workspace>"
+    echo "Usage: $0 <target-model>"
     echo "Target-Models : yolox_tiny, yolox_nano, yolox_s, yolox_m, yolox_l"
-    echo "WORKSPACE : GPU memory workspace. Default 16."
     exit 1
 fi
 
 MODEL=$1
-TRT_WORKSPACE=$2
-if [ -z "$2" ]; then
-    TRT_WORKSPACE=16
-fi
-
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 echo "Model Name: ${MODEL}"
-echo "Workspace size: ${TRT_WORKSPACE}"
 echo ""
 
 ONNX_MODEL_PATH=$SCRIPT_DIR/../onnx/$MODEL.onnx
@@ -34,4 +27,4 @@ fi
 /usr/src/tensorrt/bin/trtexec \
     --onnx=$SCRIPT_DIR/../onnx/$MODEL.onnx \
     --saveEngine=$SCRIPT_DIR/$MODEL.trt \
-    --fp16 --verbose --workspace=$((1<<$TRT_WORKSPACE))
+    --fp16 --verbose
