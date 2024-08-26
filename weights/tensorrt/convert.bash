0215033589240d1da14eac2977386560fc24cab9
fateshelled <53618876+fateshelled@users.noreply.github.com>
2024-08-27 00:37:30 +0900

    fix tensorrt convert script

--- weights/tensorrt/convert.bash
+++ weights/tensorrt/convert.bash
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-# if $1 is empty
-if [ -z "$1" ]; then
-    echo "Usage: $0 <target-model>"
-    echo "Target-Models : yolox_tiny, yolox_nano, yolox_s, yolox_m, yolox_l"
-    exit 1
-fi
+function convert {
+    # if $1 is empty
+    if [ -z "$1" ]; then
+        echo "Usage: $0 <target-model>"
+        echo "Target-Models : yolox_tiny, yolox_nano, yolox_s, yolox_m, yolox_l"
+        return
+    fi
 
-MODEL=$1
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+    MODEL=$1
+    SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-echo "Model Name: ${MODEL}"
-echo ""
+    echo "Model Name: ${MODEL}"
+    echo ""
 
-ONNX_MODEL_PATH=$SCRIPT_DIR/../onnx/$MODEL.onnx
-if [ ! -e $ONNX_MODEL_PATH ]; then
-    $SCRIPT_DIR/../onnx/download.bash $MODEL
-fi
+    ONNX_MODEL_PATH=$SCRIPT_DIR/../onnx/$MODEL.onnx
+    if [ ! -e $ONNX_MODEL_PATH ]; then
+        $SCRIPT_DIR/../onnx/download.bash $MODEL
+    fi
 
-if [ ! -e $ONNX_MODEL_PATH ]; then
-    echo "[ERROR] Not Found ${ONNX_MODEL_PATH}"
-    echo "[ERROR] Please check target model name."
-    exit 1
-fi
+    if [ ! -e $ONNX_MODEL_PATH ]; then
+        echo "[ERROR] Not Found ${ONNX_MODEL_PATH}"
+        echo "[ERROR] Please check target model name."
+        return
+    fi
 
-/usr/src/tensorrt/bin/trtexec \
-    --onnx=$SCRIPT_DIR/../onnx/$MODEL.onnx \
-    --saveEngine=$SCRIPT_DIR/$MODEL.trt \
-    --fp16 --verbose
+    /usr/src/tensorrt/bin/trtexec \
+        --onnx=$SCRIPT_DIR/../onnx/$MODEL.onnx \
+        --saveEngine=$SCRIPT_DIR/$MODEL.trt \
+        --fp16 --verbose
+}
+
+convert $1
